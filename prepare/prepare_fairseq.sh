1154d7c9b54079d460460d42a947e490a471a021
nguyendvk <dvknguyen999@gmail.com>
2022-04-16 16:50:49 +0700

    fix prepare fairseq

--- prepare/prepare_fairseq.sh
+++ prepare/prepare_fairseq.sh
@@ -1,11 +1,12 @@
 CUDA_VERSION=$1
 rm -rf fairseq
 pip install torch==1.10.1+$CUDA_VERSION torchvision==0.11.2+$CUDA_VERSION torchaudio==0.10.1 -f https://download.pytorch.org/whl/torch_stable.html
-git clone https://github.com/pytorch/fairseq.git -b v0.10.2
+git clone https://github.com/pytorch/fairseq.git
 cd fairseq
 pip install --editable .
 python setup.py build develop
 cd ../
+rm -rf apex
 git clone https://github.com/NVIDIA/apex
 cd apex
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" \
